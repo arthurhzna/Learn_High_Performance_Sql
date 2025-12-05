82e4664b51c50687d1f9aa954a9cbaa7780a7a29
Arthur Hozanna <90093341+arthurhzna@users.noreply.github.com>
2025-12-05 23:31:46 +0700

    learn Sorting Records

--- 6. Sorting Records/1.sql
+++ 6. Sorting Records/1.sql
@@ -1,1 +1,11 @@
 SELECT * FROM cities ORDER BY name;
+
+SELECT * FROM cities ORDER BY name DESC;
+
+SELECT * FROM cities ORDER BY name ASC;
+
+SELECT * FROM cities ORDER BY population DESC;
+
+SELECT * FROM cities OFFSET 2;
+
+SELECT * FROM cities LIMIT 2;
